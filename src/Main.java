433ecd2099bdb294b4c4cf98c72af35d1f575e0d
ProjectSkyapple <36686926+ProjectSkyapple@users.noreply.github.com>
2023-09-18 12:12:55 -0500

    Add menu, print auditorium, and add user seat/ticket request inputs

--- src/Main.java
+++ src/Main.java
@@ -47,5 +47,78 @@ public class Main {
 
         System.out.println("**********");
         // *******
+
+        int selectedOption = 0;
+
+        while (selectedOption != 2) {
+            System.out.println("1. Reserve Seats");
+            System.out.println("2. Exit");
+            System.out.print("Choose an option: ");
+
+            selectedOption = scnr.nextInt();
+
+            if (selectedOption == 2) {
+                break;
+            }
+
+            // Print seat letters to console.
+            System.out.print("  ");
+            for (int i = 0; i < numSeatsPerRow; i++) {
+                System.out.print((char) ('A' + i));
+            }
+
+            System.out.println();
+
+            // Print rows to console.
+            for (int i = 1; i <= numRows; i++) {
+                System.out.print(i + " ");
+
+                for (int j = 0; j < numSeatsPerRow; j++) {
+                    if (auditorium[i - 1][j] == '.') {
+                        System.out.print('.');
+                    } else {
+                        System.out.print('#');
+                    }
+                }
+
+                System.out.println();
+            }
+
+            int selectedRow = 0;
+            char selectedSeat = ' ';
+            int requestedNumAdultTickets = -1;
+            int requestedNumChildTickets = -1;
+            int requestedNumSeniorTickets = -1;
+
+            while (selectedRow < 1 || selectedRow > numRows) {
+                // Prompt for selectedRow.
+                System.out.print("Select a row: ");
+                selectedRow = scnr.nextInt();
+            }
+
+            while (selectedSeat < 'A' || selectedSeat > (char) ('A' + numSeatsPerRow - 1)) {
+                // Prompt for selectedSeat.
+                System.out.print("Select a starting seat: ");
+                selectedSeat = scnr.next().charAt(0);
+            }
+
+            while (requestedNumAdultTickets < 0) {
+                // Prompt for requestedNumAdultTickets.
+                System.out.print("Enter the number of adults: ");
+                requestedNumAdultTickets = scnr.nextInt();
+            }
+
+            while (requestedNumChildTickets < 0) {
+                // Prompt for requestedNumChildTickets.
+                System.out.print("Enter the number of children: ");
+                requestedNumChildTickets = scnr.nextInt();
+            }
+
+            while (requestedNumSeniorTickets < 0) {
+                // Prompt for requestedNumSeniorTickets.
+                System.out.print("Enter the number of seniors: ");
+                requestedNumSeniorTickets = scnr.nextInt();
+            }
+        }
     }
 }
